1fac4882516ab2f7b097223f66218bc3613ceaa5
윤재상 <banana2692@daum.net>
2021-07-09 07:54:27 +0900

    Feature read bmp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,40 @@
 #include <iostream>
 #include <memory>
 #include <random>
+#include <array>
+#include <fstream>
 
 using std::cout;
 using std::endl;
 using std::unique_ptr;
+using std::array;
+using std::ifstream;
 
 constexpr int SCREEN_WIDTH = 1024;
 constexpr int SCREEN_HEIGHT = 768;
 constexpr int BLOCK_SIZE = 4;
-constexpr int MAP_COLUMNS = SCREEN_WIDTH / BLOCK_SIZE;
-constexpr int MAP_ROWS = SCREEN_HEIGHT / BLOCK_SIZE;
+constexpr int MAP_COLUMNS = 256;
+constexpr int MAP_ROWS = 192;
+constexpr int DISTRICT_SIZE = 64;
 
-enum SystemError : int {
+static_assert(SCREEN_WIDTH == BLOCK_SIZE * MAP_COLUMNS);
+static_assert(SCREEN_HEIGHT == BLOCK_SIZE * MAP_ROWS);
+
+enum class SystemError : int {
 	FAIL_INIT_SDL=1,
 	FAIL_INIT_WINDOW,
+};
+
+struct Block {
+	enum class GeoType : int {
+		Plain,
+		Block
+	} geo = GeoType::Plain;
+	int owner = 0;
+
+	Block() = default;
+	Block(Block&) = delete;
+	Block(Block&&) = delete;
 };
 
 class Main {
@@ -25,7 +45,7 @@ class Main {
 	SDL_Surface* screen = nullptr;
 	std::mt19937_64 rnd{};
 
-	int block_state[MAP_COLUMNS][MAP_ROWS]{};
+	array<array<Block, MAP_ROWS>, MAP_COLUMNS> blocks{};
 public:
 	bool would_be_close = false;
 	uint64_t total_tick = 0;
@@ -33,9 +53,13 @@ public:
 	int init() {
 		cout << "Hello world!" << endl;
 
+		ifstream file{ "geo.bmp", std::ios::ios_base::binary };
+		uint8_t buf[80000]{};
+		file.read((char*)buf, 80000);
+
 		if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 			printf("%s\n", SDL_GetError());
-			return SystemError::FAIL_INIT_SDL;
+			return (int)SystemError::FAIL_INIT_SDL;
 		}
 
 		window = SDL_CreateWindow("SDL Tutorial",
@@ -44,37 +68,45 @@ public:
 		);
 		if (window == nullptr) {
 			printf("%s\n", SDL_GetError());
-			return SystemError::FAIL_INIT_WINDOW;
+			return (int)SystemError::FAIL_INIT_WINDOW;
 		}
 
 		screen = SDL_GetWindowSurface(window);
 
 		for (int x = 0; x < MAP_COLUMNS; ++x) {
 			for (int y = 0; y < MAP_ROWS; ++y) {
-				block_state[x][y] = rnd() % 3;
+				uint8_t B = buf[54 + (x + y * MAP_COLUMNS) * 3];
+				uint8_t G = buf[54 + (x + y * MAP_COLUMNS) * 3 + 1];
+				uint8_t R = buf[54 + (x + y * MAP_COLUMNS) * 3 + 1];
+				
+				
+
+				blocks[x][y].owner = rnd() % 3;
+				
+
+				float nx = sinf(x / 24.f);
+				float ny = sinf(y / 24.f);
+				if (B > 127)
+					blocks[x][y].geo = Block::GeoType::Block;
 			}
 		}
+
+
 		return 0;
 	}
 
 	void loop() {
-		SDL_FillRect(screen, nullptr, 0x000000);
-
-		static SDL_Rect rect{};
 		static SDL_Event _event{};
-		
-		rect.w = BLOCK_SIZE;
-		rect.h = BLOCK_SIZE;
-		
-		for (int x = 0; x < MAP_COLUMNS; ++x) {
-			for (int y = 0; y < MAP_ROWS; ++y) {
-				rect.x = x * BLOCK_SIZE;
-				rect.y = y * BLOCK_SIZE;
-				if (block_state[x][y] == 0) SDL_FillRect(screen, &rect, 0x204070);
-				else if (block_state[x][y] == 1) SDL_FillRect(screen, &rect, 0x282070);
-				else SDL_FillRect(screen, &rect, 0x206870);
+		while (SDL_PollEvent(&_event) != 0) {
+			if (_event.type == SDL_QUIT) {
+				this->would_be_close = true;
 			}
-		}
+			else if (_event.type == SDL_KEYDOWN) {
+				if (_event.key.keysym.sym == SDLK_ESCAPE)
+					this->would_be_close = true;
+			}
+		}			
+
 
 		for (int x = 0; x < MAP_COLUMNS; ++x) {
 			for (int y = 0; y < MAP_ROWS; ++y) {
@@ -83,22 +115,39 @@ public:
 					int dx = x + vector[z][0];
 					int dy = y + vector[z][1];
 					if (dx >= 0 && dx < MAP_COLUMNS && dy >= 0 && dy < MAP_ROWS) {
-						if (rnd() % 100 < 5
-							//&& (block_state[dx][dy] - block_state[x][y] + 3) % 3 == 1
-							) {
-							block_state[dx][dy] = block_state[x][y];
+						if (rnd() % 100 < 5) {
+							blocks[dx][dy].owner = blocks[x][y].owner;
 						}
 					}
 				}
 			}
 		}
 
-		SDL_PollEvent(&_event);
+	}
 
+	void draw() {
+		SDL_FillRect(screen, nullptr, 0x000000);
+
+		static SDL_Rect rect{};
+
+		rect.w = BLOCK_SIZE;
+		rect.h = BLOCK_SIZE;
+
+		for (int x = 0; x < MAP_COLUMNS; ++x) {
+			for (int y = 0; y < MAP_ROWS; ++y) {
+				rect.x = x * BLOCK_SIZE;
+				rect.y = y * BLOCK_SIZE;
+				if (blocks[x][y].geo == Block::GeoType::Block) {
+					SDL_FillRect(screen, &rect, 0x0e1038);
+				}
+				else {
+					if (blocks[x][y].owner == 0) SDL_FillRect(screen, &rect, 0x204070);
+					else if (blocks[x][y].owner == 1) SDL_FillRect(screen, &rect, 0x282070);
+					else SDL_FillRect(screen, &rect, 0x206870);
+				}
+			}
+		}
 		SDL_UpdateWindowSurface(window);
-		
-		if (GetAsyncKeyState(VK_ESCAPE))
-			this->would_be_close = true;
 		++total_tick;
 	}
 
@@ -115,6 +164,7 @@ int main(int argc, char* args[]) {
 	
 	while (!O.would_be_close) {
 		O.loop();
+		O.draw();
 	}
 	
 
